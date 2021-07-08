5f176927b6ab83b924aa3e25a4245217ca9b199b
Goutham Kumar Pagadala <gouthamkumarp@outlook.com>
2021-07-08 12:14:22 +0530

    Updated

--- src/test/java/com/sampl/bdd/manager/DriverManager.java
+++ src/test/java/com/sampl/bdd/manager/DriverManager.java
@@ -23,7 +23,7 @@ public class DriverManager {
 	public void launchDriver(BrowserType browser) {
 		switch(browser) {
 		case Chrome:
-			WebDriverManager.chromedriver().setup();
+			WebDriverManager.chromedriver().browserVersion("73.0.3683.75").setup();
 			driver = new ChromeDriver();
 			break;
 		case Firefox:
